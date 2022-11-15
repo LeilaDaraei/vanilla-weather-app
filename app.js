c84f4288e64539dbe4a132479b78b87a1c54f0a9
Leila Daraei <107926155+LeilaDaraei@users.noreply.github.com>
2022-11-15 19:08:06 +0100

    changed text color

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 let apiKey = "9f5f4t4a17f1b05b1oda4343d82d064d";
 let url = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}`;
+let urlLonLat = `https://api.shecodes.io/weather/v1/current?lon=${lon}&lat=${lat}&key=${key}`;
