d89254488fd49f87ea1b173d267c8654c9481c64
Leila Daraei <107926155+LeilaDaraei@users.noreply.github.com>
2022-11-16 10:54:49 +0100

    added axios

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-let apiKey = "9f5f4t4a17f1b05b1oda4343d82d064d";
-let url = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}`;
-let urlLonLat = `https://api.shecodes.io/weather/v1/current?lon=${lon}&lat=${lat}&key=${key}`;
+// let urlLonLat = `https://api.shecodes.io/weather/v1/current?lon=${lon}&lat=${lat}&key=${key}`;
+
+function displayTemp(response) {
+  let apiKey = "9f5f4t4a17f1b05b1oda4343d82d064d";
+  let query = "London";
+  let url = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}&units=metric`;
+}
+
+axios.get(url).then(displayTemp);
+
+function showCityTemp(event) {
+  event.preventDefault();
+  let h1Text = document.querySelector("h1");
+  let city = document.querySelector(".frm.value");
+  h1Text.innerHTML = city;
+}
+
+let submitbtn = document.querySelector("#submit");
+submitbtn.addEventListener("submit", showCityTemp);
